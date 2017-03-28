9cf3c481d89632c4b8790f2bc58cd693a063e069
Kristin Morgan <kristin.morgan@gtri.gatech.edu>
2017-03-28 09:07:12 -0400

    Updated to use Jupyter Notebook.

--- arduino_read.py
+++ arduino_read.py
@@ -1,7 +1,26 @@
-import sys, serial, argparse
+import sys, serial, argparse, csv, datetime
 from time import sleep
     
 BAUD = 9600
+
+
+def print_data(ser):
+    line = ser.readline()
+    data = [float(val) for val in line.split()]
+    print(data)
+
+def testCSV():
+    csvfile = open('test_file.csv', 'ab')
+    writer = csv.writer(csvfile, delimiter=',')
+    data = ['1','2','3']
+    writer.writerow(data)
+    csvfile.close()
+
+
+def writeToCSV(ser):
+    # Your code goes here!
+    pass
+    
 
 # main() function
 def main():
@@ -17,14 +36,12 @@ def main():
     ser = serial.Serial(args.port, BAUD)
     try:
         while True:
-            line = ser.readline()
-            data = [float(val) for val in line.split()]
-            print(data)
+            print_data(ser)
     except KeyboardInterrupt:
         print("Please don't leave!  Think of all the fun times we had together!")
     finally:
         ser.flush()
-        ser.close()    
+        ser.close()   
 
 # call main
 if __name__ == '__main__':
