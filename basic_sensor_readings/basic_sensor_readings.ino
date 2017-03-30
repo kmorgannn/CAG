a44ff53499b44d94b6e99b30e906e2969ae3ffbf
Kristin Morgan <kristin.morgan@gtri.gatech.edu>
2017-03-30 11:30:26 -0400

    Updated to match savannah’s sensor/pin configuration.

--- basic_sensor_readings/basic_sensor_readings.ino
+++ basic_sensor_readings/basic_sensor_readings.ino
@@ -1,6 +1,4 @@
-int SOIL_SENSOR_INPUT_PIN = 0;
-int TEMPERATURE_INPUT_PIN = 1;
-int LIGHT_INPUT_PIN = 2;
+int TEMPERATURE_INPUT_PIN = 0;
 int BAUD = 9600;
 int DELAY = 100;
 int TEN_BITS = 1023;
@@ -8,18 +6,14 @@ int TEN_BITS = 1023;
 void setup()
 {
   Serial.begin(BAUD); 
-  pinMode(SOIL_SENSOR_INPUT_PIN, INPUT);
   pinMode(TEMPERATURE_INPUT_PIN, INPUT);
-  pinMode(LIGHT_INPUT_PIN, INPUT);
 }
 
 void loop()
 {
   Serial.println(
     ""
-    + String(TEN_BITS - analogRead(SOIL_SENSOR_INPUT_PIN)) + " "
     + String(TEN_BITS - analogRead(TEMPERATURE_INPUT_PIN)) + " "
-    + String(TEN_BITS - analogRead(LIGHT_INPUT_PIN)) + " "
     );
 
   delay(DELAY); 
